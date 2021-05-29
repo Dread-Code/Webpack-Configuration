d1e5167dd5131670754445f27f5808d8e8003128
DreadfulCrazy <lucasospina97@gmail.com>
2021-05-29 00:03:58 -0500

    Webpack Configuration

---
+++ webpack.config.dev.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+    //Punto de entrada de la app
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]' /**COnfiguracion necesaria para las imagenes */
+    },
+    mode: 'development',
+    watch: true,
+    //Extenciones que tiene que identifacar webpack para configurar el proyecto
+    resolve:{
+        extensions:['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+
+        }
+    },
+    module:{
+        rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.woff|woff2?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit:10000,
+                        MimeType: "application/font-woff",/**EL tipo de 
+                        dato que estamos utilizando */
+                        name: "[name].[contenthash].[ext]", /** Respetar la extencion 
+                        y nombre que tiene el archivo*/
+                        outputPath: "./assets/fonts", /**Hacia donde
+                        lo estamos enviando */
+                        publicPath: "../assets/fonts", 
+                        esModule: false
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns:[
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new Dotenv()
+    ],
+}
